4f408e00c3a104eb3ee64c3da987d00a374638fb
mintsweet <0x1304570@gmail.com>
2022-04-12 13:43:57 +0800

    chore: build content optimization

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { peerDependencies } from './package.json';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
       name: 'mst-advanced',
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
     },
     sourcemap: true,
   },
