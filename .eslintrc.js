bca9c1da341bd723eb7486ef4c93d638d4cc6588
mintsweet <0x1304570@gmail.com>
2022-04-15 13:32:48 +0800

    chore: update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['mints'],
+
+  parserOptions: {
+    project: [
+      './tsconfig.json',
+      './tsconfig.node.json',
+      './examples/request-demo/tsconfig.json',
+      './examples/request-demo/tsconfig.node.json',
+    ],
+  },
 };
