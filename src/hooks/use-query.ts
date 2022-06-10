fb56ed4288021cc57eaf850d957f3c0b929a723b
mintsweet <0x1304570@gmail.com>
2022-06-10 15:59:51 +0800

    feat: optimize the performance of use-query

--- src/hooks/use-query.ts
+++ src/hooks/use-query.ts
@@ -1,17 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { ModelProperties, IModelType } from 'mobx-state-tree';
+import { isEqual } from 'lodash';
 
 import { useMst } from './use-mst';
 
+type IParams = Record<string, unknown>;
+
 export const useQuery = <PROPS extends ModelProperties, OTHERS, CustomC, CustomS>(
   Model: IModelType<PROPS, OTHERS, CustomC, CustomS>,
-  params?: Record<string, unknown>,
+  params?: IParams,
 ) => {
+  const prevParams = useRef<IParams>();
   const store = useMst(Model);
+  const { fetchData } = store;
 
   useEffect(() => {
-    store.fetchData(params);
-  }, [JSON.stringify(params)]);
+    if (!isEqual(params, prevParams.current)) {
+      fetchData(params);
+      prevParams.current = params;
+    }
+  }, [params, prevParams, fetchData]);
 
   return store;
 };
