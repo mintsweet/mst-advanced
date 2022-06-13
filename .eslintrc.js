53f29e6993feb8fcc36c2f4c20a9f9d918b1bb5c
mintsweet <0x1304570@gmail.com>
2022-06-13 11:30:15 +0800

    refactor: refresh the directory with yarn3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
   extends: ['mints'],
-
-  parserOptions: {
-    project: [
-      './tsconfig.json',
-      './tsconfig.node.json',
-      './examples/request-demo/tsconfig.json',
-      './examples/request-demo/tsconfig.node.json',
-    ],
-  },
 };
